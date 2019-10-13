8adb1ad2930554cb6b9f8de4aa89351c4b812781
Eric Liu <ericyl.us@gmail.com>
2019-10-13 11:09:44 -0700

    chore: formatting

--- tests/inlineFavicon.spec.ts
+++ tests/inlineFavicon.spec.ts
@@ -4,7 +4,7 @@ import { inlineFavicon } from '../src';
 describe('posthtml-inline-favicon', () => {
   it('matches the snapshot', () => {
     posthtml()
-      .use(inlineFavicon({ path: 'src/tests' }))
+      .use(inlineFavicon({ path: 'tests' }))
       .process(
         `<head>
           <link rel="shortcut icon" href="favicon.ico">
