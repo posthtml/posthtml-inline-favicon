bb79791f1653e1403e5090a4724e8f219ccf6472
Eric Liu <ericyl.us@gmail.com>
2024-04-19 23:55:54 -0700

    feat: upgrade `posthtml`

--- src/inlineFavicon.ts
+++ src/inlineFavicon.ts
@@ -1,23 +1,23 @@
 import Datauri from "datauri/sync";
-import path from "path";
-import { PostHTML } from "posthtml";
+import path from "node:path";
+import { type Plugin } from "posthtml";
 
-function inlineFavicon(options: IOptions = { path: "" }) {
-  return function plugin(tree: PostHTML.Node) {
+function inlineFavicon(options: IOptions = { path: "" }): Plugin<void> {
+  return function plugin(tree) {
     tree.match(
       {
         tag: "link",
         attrs: { rel: new RegExp(/icon/), href: new RegExp(/\S+/) },
       },
       (node) => {
-        const href = node.attrs!.href as string;
+        const href = node.attrs.href;
         const file = path.join(process.cwd(), options.path || "", href);
 
         const { base64 } = Datauri(file);
 
-        node.attrs!.rel = "icon";
-        node.attrs!.type = "image/png";
-        node.attrs!.href = `data:image/png;base64,${base64}`;
+        node.attrs.rel = "icon";
+        node.attrs.type = "image/png";
+        node.attrs.href = `data:image/png;base64,${base64}`;
 
         return node;
       },
